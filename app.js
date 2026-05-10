af15ece7c35312ac115f632a384e24a16b2fcc2f
bogle2512-code <bogle2512@gmail.com>
2026-05-10 21:09:14 +0900

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-console.log("AI Cinematic Workflow Director Loaded");
-
-window.addEventListener("load", () => {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("./service-worker.js")
-      .then(() => console.log("Service Worker Registered"))
-      .catch(err => console.error(err));
-  }
-});
